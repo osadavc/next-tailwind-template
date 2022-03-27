1a0daf43f27a8f0bf50c92f42c4c4eaa19621556
osadavc <osadavidath@gmail.com>
2022-03-27 11:53:09 +0530

    🎉 Basic Configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-bold">
+        Welcome To The GOAT Framework (Greatest Of All The Time)
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
